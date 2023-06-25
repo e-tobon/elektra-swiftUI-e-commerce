5100129d1608e14ecf982cf1ab541af4017fa8f8
Edgar Tobón Sosa <etbn@MacBook-Pro-de-Edgar.local>
2023-06-25 04:11:42 -0600

    svp 9

---
+++ e-commerce-elekctra-SwifIUI/Vistas/ProductoDetail.swift
@@ -0,0 +1,20 @@
+//
+//  ProductoDetail.swift
+//  e-commerce-elekctra-SwifIUI
+//
+//  Created by Edgar Tobón Sosa on 25/06/23.
+//
+
+import SwiftUI
+
+struct ProductoDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductoDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductoDetail()
+    }
+}
