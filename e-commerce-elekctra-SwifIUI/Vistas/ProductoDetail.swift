c43fbefd76a969d41e5b58b400a40fa813219873
Edgar Tobón Sosa <etbn@MacBook-Pro-de-Edgar.local>
2023-06-25 14:18:02 -0600

    imagenes

--- e-commerce-elekctra-SwifIUI/Vistas/ProductoDetail.swift
+++ e-commerce-elekctra-SwifIUI/Vistas/ProductoDetail.swift
@@ -11,12 +11,71 @@ struct ProductoDetail: View {
     let articulo:producto?
     var body: some View {
         VStack{
-            Text(articulo!.nombre!)
-                .bold()
-                .multilineTextAlignment(.center)
-                .shadow(radius: 10)
             
-            Image
+            ArticuloNombre(nombreArticulo: articulo?.nombre!)
+            ZStack{
+                imagenesArticulo(stringImagenes: articulo?.urlImagenes, index: 0)
+                    .frame(maxHeight: .infinity,alignment: .top)
+                if(articulo?.porcentajeDescuento! != 0){
+                    porcentajeDescuento(porcentaje: articulo?.porcentajeDescuento!)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                        .frame(maxHeight: .infinity,alignment: .top)
+                }
+            }
+            
+            
+            
+            
+        
+        }
+    }
+}
+
+struct ArticuloNombre: View{
+    var nombreArticulo:String?
+    var body: some View{
+        Text(nombreArticulo!)
+            .bold()
+            .multilineTextAlignment(.center)
+            .shadow(radius: 10)
+    }
+}
+
+struct imagenesArticulo:View{
+    var stringImagenes:[String]?
+    var index:Int
+    var body: some View{
+        AsyncImage(url: URL(string: stringImagenes![index] )) { image in
+            image.resizable()
+                .aspectRatio(contentMode: .fit)
+            .frame(maxWidth: 400, maxHeight: 350)
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
+struct porcentajeDescuento: View{
+    var porcentaje:Double?
+    var body: some View{
+        VStack{
+            Text("Hasta")
+                .bold()
+                .shadow(radius: 5)
+                .foregroundColor(.red)
+                .font(.system(size:13))
+            Text("\(porcentaje?.redondear(numeroDeDecimales: 1) ?? "")%")
+                .bold()
+                .shadow(radius: 5)
+                .foregroundColor(.red)
+                .font(.system(size:18))
+            Text("de descuento")
+                .bold()
+                .shadow(radius: 5)
+                .foregroundColor(.red)
+                .font(.system(size: 13))
+                .padding(.leading)
+            
         }
     }
 }
